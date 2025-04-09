c8ccf0eace9cddb19d605beec22ea67c33e7b643
FixSann <168392873+FixSann@users.noreply.github.com>
2025-04-09 13:11:54 +0700

    Add files via upload

---
+++ 05_Generic/TP/tpmodul5_2311104064/tpmodul5_2311104064/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Memanggil method SapaUser dari class HaloGeneric
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Ikhsan");
+
+        // Menggunakan class DataGeneric untuk menyimpan dan mencetak data NIM
+        DataGeneric<string> data = new DataGeneric<string>("2311104064");
+        data.PrintData();
+    }
+}
