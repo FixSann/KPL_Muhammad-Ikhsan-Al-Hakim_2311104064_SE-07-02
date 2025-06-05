4d6aa861bcc3cf55547cdf38a1618ccb365c1104
FixSann <168392873+FixSann@users.noreply.github.com>
2025-06-05 11:55:17 +0700

    Add files via upload

---
+++ 09_API_Design_dan_Construction_Using_Swagger/tpmodul9_2311104064/tpmodul9_2311104064/Controllers/MahasiswaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul9_2311104064.Models;
+
+namespace tpmodul9_2311104064.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MahasiswaController : ControllerBase
+{
+    private static List<Mahasiswa> daftarMahasiswa = new()
+    {
+        new Mahasiswa { Nama = "Muhammad", Nim = "2311" },
+        new Mahasiswa { Nama = "Ikhsan", Nim = "104" },
+        new Mahasiswa { Nama = "Al-Hakim", Nim = "064" }
+
+    };
+
+    [HttpGet]
+    public ActionResult<List<Mahasiswa>> GetAll() => daftarMahasiswa;
+
+    [HttpGet("{index}")]
+    public ActionResult<Mahasiswa> GetByIndex(int index)
+    {
+        if (index < 0 || index >= daftarMahasiswa.Count) return NotFound();
+        return daftarMahasiswa[index];
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromBody] Mahasiswa mhs)
+    {
+        daftarMahasiswa.Add(mhs);
+        return Ok("Mahasiswa ditambahkan.");
+    }
+
+    [HttpDelete("{index}")]
+    public IActionResult Delete(int index)
+    {
+        if (index < 0 || index >= daftarMahasiswa.Count) return NotFound();
+        daftarMahasiswa.RemoveAt(index);
+        return Ok("Mahasiswa dihapus.");
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/tpmodul9_2311104064/tpmodul9_2311104064/Models/Mahasiswa.cs
@@ -0,0 +1,7 @@
+﻿namespace tpmodul9_2311104064.Models;
+
+public class Mahasiswa
+{
+    public string Nama { get; set; }
+    public string Nim { get; set; }
+}
