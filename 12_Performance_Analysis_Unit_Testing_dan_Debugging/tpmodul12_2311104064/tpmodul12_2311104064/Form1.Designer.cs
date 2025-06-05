3eab9f3c67b70e7001a4513d3e9ee2ccf5a2053e
FixSann <168392873+FixSann@users.noreply.github.com>
2025-06-05 19:52:10 +0700

    add

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/tpmodul12_2311104064/tpmodul12_2311104064/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace tpmodul12_2311104064
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.cekButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(352, 79);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(100, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // cekButton
+            // 
+            this.cekButton.Location = new System.Drawing.Point(352, 120);
+            this.cekButton.Name = "cekButton";
+            this.cekButton.Size = new System.Drawing.Size(97, 23);
+            this.cekButton.TabIndex = 1;
+            this.cekButton.Text = "Cek Tanda";
+            this.cekButton.UseVisualStyleBackColor = true;
+            this.cekButton.Click += new System.EventHandler(this.cekButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(394, 212);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(11, 16);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.cekButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button cekButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
